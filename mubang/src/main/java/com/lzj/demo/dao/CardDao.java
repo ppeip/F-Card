ae07397a7a97525184b33660ee4148c3ea5a4f6e
SitAndForget <97599487+1zuoAn@users.noreply.github.com>
2022-11-11 22:50:30 +0800

    修改了一些由于数据库变更的bug，完善了抽卡业务逻辑
    具体改动的部分 PersonalCard，PersonalCardServicempl，PersonalCardDao.xml等4个xml文件

---
+++ mubang/src/main/java/com/lzj/demo/dao/CardDao.java
@@ -0,0 +1,22 @@
+package com.lzj.demo.dao;
+
+import com.lzj.demo.entity.Card;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @author 林子键
+ * @version 1.0
+ */
+@Mapper
+@Repository
+public interface CardDao {
+    List<Card> queryCard();
+    Card queryCardByName(String cardName);
+    int insertCard(Card card);
+    int updateCard(Card card);
+    int deleteCard(Card card);
+
+}
