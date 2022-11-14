59e903b01754cb84f0c0492e81c94138931a09c1
SitAndForget <97599487+1zuoAn@users.noreply.github.com>
2022-11-14 21:04:15 +0800

    木有改动

---
+++ DB/mybatis.sql
@@ -0,0 +1,149 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 80031
+Source Host           : localhost:3306
+Source Database       : mybatis
+
+Target Server Type    : MYSQL
+Target Server Version : 80031
+File Encoding         : 65001
+
+Date: 2022-11-12 17:08:50
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for card
+-- ----------------------------
+DROP TABLE IF EXISTS `card`;
+CREATE TABLE `card` (
+  `ability1` int DEFAULT NULL,
+  `ability2` int DEFAULT NULL,
+  `ability3` int DEFAULT NULL,
+  `ability4` int DEFAULT NULL,
+  `ability5` int DEFAULT NULL,
+  `cardLevel` int DEFAULT NULL,
+  `experience` int DEFAULT NULL,
+  `rank` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `cardName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `introduction` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `preferance` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`cardName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of card
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for cardinhand
+-- ----------------------------
+DROP TABLE IF EXISTS `cardinhand`;
+CREATE TABLE `cardinhand` (
+  `ability1` int DEFAULT NULL,
+  `ability2` int DEFAULT NULL,
+  `ability3` int DEFAULT NULL,
+  `ability4` int DEFAULT NULL,
+  `ability5` int DEFAULT NULL,
+  `cardName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `rank` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `introduction` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `preferance` varchar(300) DEFAULT NULL,
+  `UID` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `experience` int DEFAULT NULL,
+  `cardLevel` int DEFAULT NULL,
+  PRIMARY KEY (`UID`,`cardName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of cardinhand
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for personalcard
+-- ----------------------------
+DROP TABLE IF EXISTS `personalcard`;
+CREATE TABLE `personalcard` (
+  `ability1` int DEFAULT NULL,
+  `ability2` int DEFAULT NULL,
+  `ability3` int DEFAULT NULL,
+  `ability4` int DEFAULT NULL,
+  `ability5` int DEFAULT NULL,
+  `cardLevel` int DEFAULT NULL,
+  `experience` int DEFAULT NULL,
+  `rank` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `cardName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `introduction` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `preferance` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `situation` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `UID` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `dateTime` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `duplicateCards` int DEFAULT NULL,
+  PRIMARY KEY (`UID`,`cardName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of personalcard
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for question
+-- ----------------------------
+DROP TABLE IF EXISTS `question`;
+CREATE TABLE `question` (
+  `questionAbility1` float DEFAULT NULL,
+  `questionAbility2` float DEFAULT NULL,
+  `questionAbility3` float DEFAULT NULL,
+  `questionAbility4` float DEFAULT NULL,
+  `questionAbility5` float DEFAULT NULL,
+  `questionName` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `preferance` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`questionName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of question
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for rank
+-- ----------------------------
+DROP TABLE IF EXISTS `rank`;
+CREATE TABLE `rank` (
+  `collegeInfluence` int DEFAULT NULL,
+  `grade` int DEFAULT NULL,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `rank` int DEFAULT NULL,
+  `college` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `dateTime` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of rank
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `collegeInflunce` int DEFAULT NULL,
+  `coin` int DEFAULT NULL,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `college` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `phoneNumber` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `UID` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `dateTime` datetime(6) DEFAULT NULL,
+  `task` varchar(3000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `towerNumber` int DEFAULT NULL,
+  PRIMARY KEY (`UID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
